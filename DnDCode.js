37c26e65dbaee52dcb54ecf101e8560f18ee05b8
stephen-boxerman <42850698+stephen-boxerman@users.noreply.github.com>
2018-10-11 15:29:13 -0400

    Finished seting up positions for visual elements.  Now working on functional elements.

--- DnDCode.js
+++ DnDCode.js
@@ -2,8 +2,19 @@
 
 "use strict";
 
-var button = document.getElementById('CB1');
-button.addEventListener('click', function()
+var classButtons = [];
+var classes = ["Cleric","Druid", "Fighter", "Mist Walker","Monk","Paladin","Ranger","Sorcerer","Wizard","Warlock"];
+
+function start_game()
 {
-	alert('button was clicked');
-});
+	for(var i = 1; i < 11; i++)
+	{
+		console.log("CB" + i)
+		var button = document.getElementById("CB" + i);
+		button.innerHTML = classes[i - 1];
+		classButtons.push(button);
+		button.style.visibility = "hidden";
+	}
+}
+
+start_game();
